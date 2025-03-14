76714b49da28637917368a4bf2094bdfa1fd529f
ABHISEK PANDA <117925314+abhisek2004@users.noreply.github.com>
2025-03-14 23:41:58 +0530

    some changes

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask_bcrypt import Bcrypt
-from flask import Flask,render_template, request,redirect
+from flask import Flask, render_template, request, redirect
 from modules.log_out import logout_bp
 from modules.admin import admin_blueprint
 from modules.superadmin import superadmin_blueprint
 from modules.doctor import doctor_blueprint
-from modules.db import contact_collection,feedback_collection
+from modules.db import contact_collection, feedback_collection
 from modules.user import user_blueprint
+import os
 
 app = Flask(__name__)
 app.register_blueprint(logout_bp)
@@ -17,9 +18,10 @@ bcrypt = Bcrypt()
 app.secret_key = "anything_secret_is_good"
 
 
-@app.route('/',methods=['GET'])
+@app.route('/', methods=['GET'])
 def home():
     return render_template('index.html')
+
 
 @app.route('/contact', methods=['GET', 'POST'])
 def landing():
@@ -35,13 +37,14 @@ def landing():
             'name': name,
             'email': email,
             'number': number,
-            'city':city,
-            'nearest_hospital':nearest_hospial,
-            'message':message,
-            'subject':subject
+            'city': city,
+            'nearest_hospital': nearest_hospial,
+            'message': message,
+            'subject': subject
         }
         contact_collection.insert_one(contact_data)
     return render_template('contact.html')
+
 
 @app.route('/all_doctor')
 def all_doc():
@@ -57,25 +60,27 @@ def confirmation():
 def detail():
     return render_template('inv_stock_product.html')
 
-@app.route('/home_feedback',methods=['POST'])
-def feedback():
-       name = request.form['name']  
-       email = request.form['email']
-       city = request.form['city']
-       nearest_hospital = request.form['nearest_hospital']
-       message = request.form['message']
-       
-       data = {
-           'name':name,
-           'email':email,
-           'city':city,
-           'nearest_hospital':nearest_hospital,
-           'message':message
-       }
-       feedback_collection.insert_one(data) 
-       return redirect('/')
 
-@app.route('/blog',methods=['GET','POST'])
+@app.route('/home_feedback', methods=['POST'])
+def feedback():
+    name = request.form['name']
+    email = request.form['email']
+    city = request.form['city']
+    nearest_hospital = request.form['nearest_hospital']
+    message = request.form['message']
+
+    data = {
+        'name': name,
+        'email': email,
+        'city': city,
+        'nearest_hospital': nearest_hospital,
+        'message': message
+    }
+    feedback_collection.insert_one(data)
+    return redirect('/')
+
+
+@app.route('/blog', methods=['GET', 'POST'])
 def blog_single():
     return render_template('blog-single.html')
 
@@ -83,7 +88,11 @@ def blog_single():
 # a customized error handler
 @app.errorhandler(404)
 def page_not_found(error):
-    return render_template('404.html'),404
+    return render_template('404.html'), 404
+
+# if __name__ == '__main__':
+#     app.run(port=8000, debug=True)
+
 
 if __name__ == '__main__':
-    app.run(port=8000, debug=True)
+    app.run(host='0.0.0.0', port=int(os.getenv('PORT', 8000)))
