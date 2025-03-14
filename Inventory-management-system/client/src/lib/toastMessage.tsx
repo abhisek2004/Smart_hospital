f8a499bc3144bcff2a1a278bb735b494e07389de
ABHISEK PANDA <117925314+abhisek2004@users.noreply.github.com>
2025-03-14 23:27:08 +0530

    file deleted

--- Inventory-management-system/client/src/lib/toastMessage.tsx
+++
@@ -1,7 +0,0 @@
-import Swal, { SweetAlertOptions } from 'sweetalert2';
-
-const toastMessage = (props: SweetAlertOptions) => {
-  Swal.fire(props);
-};
-
-export default toastMessage;
--- Inventory-management-system/client/src/utils/decodeToken.ts
+++
@@ -1,8 +0,0 @@
-import { jwtDecode } from 'jwt-decode'
-import { TUser } from '../redux/services/authSlice'
-
-const decodeToken = (token: string): TUser => {
-  return jwtDecode(token)
-}
-
-export default decodeToken
